77ea74a4fb8c05ef21562c839bc5aa6a1aad5e26
Jesus Laucho <contacto@lauchoit.com>
2025-06-14 08:48:10 -0400

    Remove vendor directory from tracking

---
+++ src/generate/GenerateUpdateRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Lauchoit\LaravelHexMod\generate;
+
+class GenerateCreateRequest
+{
+    /**
+     * @param array $fields
+     * @param array|bool|string $content
+     * @return array|bool|string|string[]
+     */
+    public static function run(array $fields, array|bool|string $content): string|array|bool
+    {
+        $rules = collect($fields)->map(function ($field) {
+            $name = explode(':', $field)[0];
+            $rules = 'required';
+            $type = explode(':', $field)[1] ?? 'string';
+            if ($type === 'string') {
+                $rules .= '|string|max:255';
+            } elseif ($type === 'integer') {
+                $rules .= '|integer';
+            } elseif ($type === 'float') {
+                $rules .= '|numeric';
+            } elseif ($type === 'date') {
+                $rules .= '|date';
+            } elseif ($type === 'datetime') {
+                $rules .= '|date_format:Y-m-d H:i:s';
+            } elseif ($type === 'json') {
+                $rules .= '|json';
+            } elseif ($type === 'boolean') {
+                $rules .= '|boolean';
+            } elseif ($type === 'text') {
+                $rules .= '|string';
+            } elseif ($type === 'longText') {
+                $rules .= '|string';
+            }
+            return "            '{$name}' => '{$rules}',";
+        })->implode("\n");
+
+        $rulesWrapped = "return [\n" . $rules . "\n        ];";
+
+        $content = str_replace('{{validationRules}}', $rulesWrapped, $content);
+        return $content;
+    }
+}
