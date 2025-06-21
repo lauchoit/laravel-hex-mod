03b6c60cd3a411561742cd1627639f6f59241cf6
Jesus Laucho <contacto@lauchoit.com>
2025-06-21 13:57:14 -0400

    Fixed test with snakeCase

--- src/generate/Test/Feature/GenerateTestFeatureUpdateById.php
+++ src/generate/Test/Feature/GenerateTestFeatureUpdateById.php
@@ -12,8 +12,25 @@ class GenerateTestFeatureUpdateById
 
         $updatedData = $filtered->map(function ($field, $i) {
             [$name, $type] = explode(':', $field);
+            $camelName = Str::camel($name);
             $value = self::exampleUpdateValue($type, $i);
-            return "            '" . $name . "' => " . var_export($value, true) . ",";
+            return "            '" . $camelName . "' => " . var_export($value, true) . ",";
+        })->implode("\n");
+
+        // Usar camelCase para request
+        $snakeNameData = $filtered->map(function ($field, $i) {
+            [$name, $type] = explode(':', $field);
+            $snakeNameDatabase = Str::snake($name);
+            $value = self::exampleUpdateValue($type, $i);
+            return "            '" . $snakeNameDatabase . "' => " . var_export($value, true) . ",";
+        })->implode("\n");
+
+        // Database assertion: snake_case keys
+        $databaseData = $filtered->map(function ($field, $i) {
+            [$name, $type] = explode(':', $field);
+            $snakeName = Str::snake($name);
+            $value = self::exampleUpdateValue($type, $i);
+            return "            '" . $snakeName . "' => " . var_export($value, true) . ",";
         })->implode("\n");
 
         // Obtener un campo aleatorio
@@ -25,18 +42,20 @@ class GenerateTestFeatureUpdateById
         $assertDatabaseHasPartial = "            'id' => \${$camelName}->id,\n";
         $filtered->each(function ($field) use (&$assertDatabaseHasPartial, $onlyOneKey, $onlyOneValue, $camelName) {
             $name = explode(':', $field)[0];
+            $snake = Str::snake($name);
             if ($name !== $onlyOneKey) {
-                $assertDatabaseHasPartial .= "            '" . $name . "' => \${$camelName}->{$name},\n";
+                $assertDatabaseHasPartial .= "            '" . $snake . "' => \${$camelName}->{$name},\n";
             } else {
-                $assertDatabaseHasPartial .= "            '" . $name . "' => " . var_export($onlyOneValue, true) . ",\n";
+                $assertDatabaseHasPartial .= "            '" . $snake . "' => " . var_export($onlyOneValue, true) . ",\n";
             }
         });
 
         $assertDatabaseMissing = "            'id' => \${$camelName}->id,\n";
-        $assertDatabaseMissing .= "            '" . $onlyOneKey . "' => " . var_export($onlyOneValue, true) . ",\n";
+        $assertDatabaseMissing .= "            '" . Str::snake($onlyOneKey) . "' => " . var_export($onlyOneValue, true) . ",\n";
 
         $jsonFields = $filtered->map(function ($field) {
-            return "                '" . explode(':', $field)[0] . "',";
+            $camelName = Str::camel(explode(':', $field)[0]);
+            return "                '" .$camelName . "',";
         })->prepend("                'id',")
             ->push("                'createdAt',")
             ->push("                'updatedAt',")
@@ -52,6 +71,8 @@ class GenerateTestFeatureUpdateById
                 '{{assertDatabaseMissing}}',
                 '{{jsonFields}}',
                 '{{snakeName}}',
+                '{{databaseData}}',
+                '{{snakeNameData}}',
             ],
             [
                 $camelName,
@@ -62,6 +83,8 @@ class GenerateTestFeatureUpdateById
                 $assertDatabaseMissing,
                 $jsonFields,
                 $snakeName,
+                $databaseData,
+                $snakeNameData,
             ],
             $content
         );
@@ -74,8 +97,9 @@ class GenerateTestFeatureUpdateById
             'integer' => 99 + $index,
             'float' => 99.99 + $index,
             'boolean' => $index % 2 === 0,
-            'date', 'datetime' => '2025-07-01',
-            'json' => ['updated' => true],
+            'date' => '2025-07-01',
+            'datetime' => '2025-07-01 12:00:00',
+            'json' => json_encode(['updated' => true]),
             default => 'Updated',
         };
     }
